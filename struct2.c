456bb94f8756c65fb5609ede0f7212f2d9bda7c8
SAGAR-B-99 <sagarbennur9606@gmail.com>
2024-11-13 13:10:35 +0530

    Add files via upload

--- struct2.c
+++ struct2.c
@@ -5,18 +5,17 @@ struct str1{
     int peri;
 };
  int circle(){
-    struct str1 a;
+    struct str1 a1;
     double  pi=3.14;
-    a.area=pi*(r*r);
-    a.peri=2*pi*r;
-    printf("%d/n%d",a.area,a.peri);
+    a1.area=pi*(r*r);
+    a1.peri=2*pi*r;
+    printf("%d/n%d",a1.area,a1.peri);
  }
  int square()
  {
-    struct str1 a;
+    struct str1 a2;
     int a;
-    a.area=a*a;
-    a.peri=4*a;
-     printf("%d/n%d",a.area,a.peri);
+    a2.area=a*a;
+    a2.peri=4*a;
+     printf("%d/n%d",a2.area,a2.peri);
  }
- int  
